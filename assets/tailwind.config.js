e0c34c4be0df007f468be6c72e413622b07609bd
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2022-09-12 11:41:24 -0300

    feat: add alpine js

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,12 +1,19 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './js/**/*.js',
     '../lib/*_web.ex',
     '../lib/*_web/**/*.*ex',
+    "../deps/petal_components/**/*.*ex",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.purple,
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 }
